a030929f25410bf2b9dc085c4f12cf8c61283e4c
Harshvardhan Dhankhar <harshvardhan@Harshvardhans-MacBook-Pro.local>
2026-04-07 21:36:38 +0530

    Added Forecast Functionality

---
+++ JS/forecast.js
@@ -0,0 +1,35 @@
+const apiKey = "baa30fa1b6a77a6d05d4b0e3c209e07a";
+const forecastDiv = document.getElementById("forecast")
+
+const params = new URLSearchParams(window.location.search);
+const city = params.get("city");
+
+getForecast(city);
+
+async function getForecast(city) {
+    forecastDiv.innerHTML = "Loading...";
+
+    const res = await fetch(
+        `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`
+    );
+
+    const data = await res.json();
+
+    showForecast(data.list);
+}
+
+function showForecast(list) {
+    forecastDiv.innerHTML = `<h2>${city} Forecast</h2>
+                            <br>`;
+
+    list.slice(0, 8).map(item => {
+        const div = document.createElement("div");
+
+        div.innerHTML = `
+            <p>${item.dt_txt}</p>
+            <p>${item.main.temp}°C</p>
+        `;
+
+        forecastDiv.appendChild(div);
+    });
+}
